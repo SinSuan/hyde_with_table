c33a7df2965103060d36f3c3299cbaea764c5ec1
SinSuan <vicky0002@gmail.com>
2024-03-15 12:10:28 +0000

    抓好恩德的文章

--- code/module/about_BM25.py
+++ code/module/about_BM25.py
@@ -16,31 +16,6 @@ def init_BM25():
     return SEARCHER
 
 
-# def call_BM25(SEARCHER, search_key):
-#     """123
-#     """
-#     print("enter call_BM25")
-
-#     TOP_K = os.getenv("TOP_K")
-#     TOP_K = int(TOP_K)
-#     total_hit = SEARCHER.search(search_key, TOP_K)
-#     print(f"\ttotal_hit =\n\t\t{total_hit}")
-#     # total_docid = [hit.docid for hit in total_hit]
-#     total_docid = []
-#     total_doc = []
-#     for hit in total_hit:
-#         print(f"\thit =\n\t\t{hit}")
-
-#         doc_id = hit.docid
-#         total_docid.append(doc_id)
-
-#         raw_doc = SEARCHER.doc(doc_id).raw()
-#         content = json.loads(raw_doc)['contents']
-#         total_doc.append(content)
-
-#     print("exit call_BM25")
-#     return total_docid, total_doc
-
 def call_BM25(total_query):
     """123
     """
--- code/try_hyde.py
+++ code/try_hyde.py
@@ -10,25 +10,11 @@ confi.ini
 import os
 import json
 from datetime import datetime
-# from pyserini.search.lucene import LuceneSearcher
 
 import pytz
-# import requests
 from dotenv import load_dotenv
 from module.about_model import init_model, call_model
-from module.about_BM25 import init_BM25, call_BM25
-
-# abuot data
-# # 存放 raw json 的 "資料夾"
-# DATAPATH_2_RAW_JSON = \
-#     "/user_data/DG/hyde_with_table/BM25/Agri_inverted_agri_table_only_113-0309/index"
-
-
-# # about prompt
-# DATAPATH_2_PROMPT = "/user_data/DG/hyde_with_table/prompt/prompt.json"
-
-# # DATAPATH_2_PSEUDO_QUERY = "/user_data/DG/hyde_with_table/prompt/psuedo_query.json"
-# DATAPATH_2_PSEUDO_QUERY = "/user_data/DG/hyde_with_table/prompt/sheila.json"
+from module.about_BM25 import call_BM25
 
 
 # about model
@@ -40,7 +26,7 @@ BOT = "breeze"
 # about time
 
 # 取得當下的 UTC 時間
-# UTC_NOW = datetime.utcnow()
+# UTC_NOW = datetime.utcnow() # 舊的
 UTC_NOW = datetime.now()
 
 # 設定 UTC+8 的時區
@@ -64,7 +50,7 @@ def create_full_prompt(user_prompt) -> str:
         system_prompt = total_prompt[BOT]
         print("\tend dealing file")
 
-    full_prompt = f"<s> {system_prompt} [INST] 依照你的知識回答：{user_prompt} [/INST]"
+    full_prompt = f"<s> {system_prompt} [INST] 幫我解釋以下表格：{user_prompt} [/INST] "
 
     print("exit create_full_prompt")
     return full_prompt
@@ -115,22 +101,15 @@ def exam_hyde_query(model_and_tokenizer):
 
     datapath = os.getenv("DATAPATH_2_PSEUDO_QUERY")
     with open(datapath, 'r', encoding='utf-8') as file:
-        print("\t\tstart dealing PSEUDO_QUERY")
         total_query = json.load(file)
-        print("\t\tend dealing PSEUDO_QUERY")
 
-    print(f"before total_query = {total_query}")
     total_query = [query['query'] for query in total_query]
-    print(f"after total_query = {total_query}")
     total_hyde_query = []
     for query in total_query:
-        print(f"before query = {query}")
         full_prompt = create_full_prompt(query)
-        print(f"after query = {query}")
         hyde_query = call_model(BOT, full_prompt, model_and_tokenizer)
-        print(f"after hyde_query = {hyde_query}")
         total_hyde_query.append(hyde_query)
-    
+
     print(f"after total_hyde_query = {total_hyde_query}")
 
     original_query_logging = new_logging(total_query)
@@ -159,11 +138,9 @@ def main() -> None:
     print("enter main")
 
     model_and_tokenizer = init_model(BOT)
-
     exam_hyde_query(model_and_tokenizer)
 
     print("exit main")
-
 
 
 if __name__ == "__main__":
